a9151934c6cb2996ac1d7f53f9f98e4eef6b9ec4
Andriy Minenko <icandriy.minenko@americanwell.com>
2022-09-07 18:50:29 +0200

    Added EBS flow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Mornig over there!!!
+          God Mornig over there!!!
         </a>
       </header>
     </div>
